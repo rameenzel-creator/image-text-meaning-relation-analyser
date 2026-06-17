6eb67942a5bb99c5cdd47100db0dda767d978073
rameenzel-creator <rameenzel@github.com>
2026-06-17 12:57:33 +0500

    Fix: Change base URL to root for Vercel deployment compatibility

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/image-text-meaning-relation-analyser/',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
